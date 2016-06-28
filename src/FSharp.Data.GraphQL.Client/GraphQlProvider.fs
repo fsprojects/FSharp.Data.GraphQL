502aa2e2d7f31db584c59a524ff421742a49df12
Alfonso Garcia-Caro <alfonso.garcia-caro@hotmail.com>
2016-06-28 16:51:10 +0200

    Parse queries for validation

--- src/FSharp.Data.GraphQL.Client/GraphQlProvider.fs
+++ src/FSharp.Data.GraphQL.Client/GraphQlProvider.fs
@@ -19,6 +19,7 @@ open QuotationHelpers
 
 module Util =
     open System.Text.RegularExpressions
+    open FSharp.Data.GraphQL
 
     let requestSchema (url: string) =
         async {
@@ -70,7 +71,7 @@ module Util =
         | _ ->
             (token :?> JValue).Value
 
-    let launchQuery (serverUrl: string) (query: string) =
+    let launchQuery (serverUrl: string) (query: string) (queryName: string) =
         async {
             use client = new WebClient()
             let queryJson = Map["query", query] |> JsonConvert.SerializeObject
@@ -78,10 +79,32 @@ module Util =
             let res = JToken.Parse json |> jsonToObject :?> IDictionary<string,obj>
             if res.ContainsKey("errors") then
                 res.["errors"] :?> string[] |> String.concat "\n" |> failwith
-            // TODO: Find a more structured way to get the query name
-            let queryName = Regex.Match(query, "^.*?(\w+)").Groups.[1].Value
-            return (res.["data"] :?> IDictionary<string,obj>).[queryName]
+            let data = res.["data"]
+            return
+                // Using options gives problems with quotations and provided methods
+                // so we have to use null here instead
+                match queryName with
+                | null -> data
+                | queryName -> (res.["data"] :?> IDictionary<string,obj>).[queryName]
         }
+
+    let getQueryName (serverUrl: string) (query: string) =
+        // This will fail if the query is not well formed
+        let doc = Parser.parse query
+        (None, doc.Definitions) ||> List.fold (fun queryOp def ->
+            match queryOp, def with
+            | None, Ast.OperationDefinition({ OperationType = Ast.Query } as q) -> Some q 
+            | _ -> queryOp)
+        |> function
+        | None -> failwith "Cannot find query operation"
+        | Some queryOp ->
+            match queryOp.SelectionSet with
+            | [Ast.Field { Name=name}] -> name // TODO: Check other possibilities
+            | _ -> null
+        // TODO: Make a first flight to be sure the query is accepted by the server?
+//        launchQuery serverUrl query |> Async.Catch |> Async.RunSynchronously |> function
+//        | Choice1Of2 _ -> ()
+//        | Choice2Of2 ex -> failwith ex.Message
 
     let createStaticMethod (tdef: ProvidedTypeDefinition) (resType: Type) (serverUrl) =
         let asyncType = typeof<Async<obj>>.GetGenericTypeDefinition().MakeGenericType(resType)
@@ -90,13 +113,10 @@ module Util =
         m.DefineStaticParameters(sargs, fun methName parameterValues ->
             match parameterValues with 
             | [| :? string as query |] ->
-                // Make a first flight to be sure the query is accepted by the server
-                launchQuery serverUrl query |> Async.Catch |> Async.RunSynchronously |> function
-                | Choice1Of2 _ -> ()
-                | Choice2Of2 ex -> failwith ex.Message
+                let queryName = getQueryName serverUrl query
                 let m2 = ProvidedMethod(methName, [], asyncType, IsStaticMethod = true) 
                 m2.InvokeCode <- fun _ ->
-                    <@@ launchQuery serverUrl query @@>
+                    <@@ launchQuery serverUrl query queryName @@>
                 tdef.AddMember m2
                 m2
             | _ -> failwith "unexpected parameter values")
