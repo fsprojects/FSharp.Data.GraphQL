cbaac3bbab25e7f5b97ed7be63367596a27ba57b
Alfonso Garcia-Caro <alfonso.garcia-caro@hotmail.com>
2016-08-05 19:34:45 +0200

    File structure

---
+++ samples/todo-graphql/client.fsx
@@ -0,0 +1,3 @@
+﻿#r "../../bin/FSharp.Data.GraphQL.Client/FSharp.Data.GraphQL.Client.dll"
+#r "node_modules/fable-core/Fable.Core.dll"
+
---
+++ samples/todo-graphql/server.fsx
@@ -0,0 +1,11 @@
+#r "../../packages/Suave/lib/net40/Suave.dll"
+#r "../../packages/Newtonsoft.Json/lib/net40/Newtonsoft.Json.dll"
+#r "../../packages/System.Runtime/lib/net462/System.Runtime.dll"
+#r "../../packages/Hopac/lib/net45/Hopac.dll"
+#r "../../packages/Hopac/lib/net45/Hopac.Core.dll"
+#r "../../packages/Hopac/lib/net45/Hopac.Platform.dll"
+#r "../../packages/FSharp.Data.GraphQL.Server/lib/FSharp.Data.GraphQL.Shared.dll"
+#r "../../packages/FSharp.Data.GraphQL.Server/lib/FSharp.Data.GraphQL.dll"
+
+open System
+
