27a6ae5373e3afe97a51a816f4c3ffccd3ccc37d
Reed Mullanix <reed.mullanix@mybazinga.com>
2017-05-30 15:08:34 -0700

    Add observablesource implementation

--- src/FSharp.Data.GraphQL.Server/ObservableExtensions.fs
+++ src/FSharp.Data.GraphQL.Server/ObservableExtensions.fs
@@ -17,3 +17,26 @@ module internal Observable =
                 observer.OnCompleted()     
                 {   new IDisposable with member __.Dispose() = ()   }
     }
+
+open System.Collections.Concurrent
+
+/// Wrapper type for IObservable that exposes event triggering
+type ObservableSource<'T>() = 
+    let mutable (key:uint64) = 0UL
+    let subscriptions = new ConcurrentDictionary<uint64, IObserver<'T>>()
+    let next o = subscriptions |> Seq.iter(fun (KeyValue(_, v)) -> v.OnNext(o))
+    let obs = 
+        { new IObservable<'T> with
+            member this.Subscribe(o) =
+                let key' = key
+                key <- key + 1UL
+                subscriptions.AddOrUpdate(key, o, (fun _ _ -> o)) |> ignore
+                { new IDisposable with
+                    member this.Dispose() =
+                        subscriptions.TryRemove(key') |> ignore } }
+    
+    /// Dispatches an event to all subscribers
+    member this.Next(o) = next o
+    
+    /// Access the underlying IObservable
+    member this.Observable = obs
