5dc6b0046941e99e530f6eb1a4db2c8bbc28f097
Ismael Carlos Velten <ismaelcarlosvelten@gmail.com>
2018-03-21 20:03:34 -0300

    Adding more samples to the apollo client query runner

--- samples/apollo-client/src/QueryRunner.js
+++ samples/apollo-client/src/QueryRunner.js
@@ -5,7 +5,7 @@ import { InMemoryCache } from 'apollo-cache-inmemory';
 import { SubscriptionClient } from 'subscriptions-transport-ws';
 import gql from 'graphql-tag';
 
-const defaultQuery = `# This is a sample query with defer directive.
+const deferQuery = `# This is a sample query with defer directive.
 # The GraphQL Giraffe server is configured to return deferred results after 5 seconds,
 # so you can observe better how the effects of a defer directive works.
 
@@ -22,11 +22,32 @@ query TestQuery {
     }
 }`;
 
+const streamQuery = `# This is a sample query with stream and defer directive.
+# The GraphQL Giraffe server is configured to return each result after 5 seconds,
+# so you can observe better how the effects of stream and defer directives work.
+
+query TestQuery {
+    hero(id:"1000") {
+        name,
+        appearsIn,
+        homePlanet @defer,
+        friends @stream {
+            ... on Human {
+              name
+            }
+          	... on Droid {
+              name
+              primaryFunction
+            }
+        }
+    }
+}`
+
 export default class QueryRunner extends React.Component {
     constructor(props) {
         super(props);
         this.state = { 
-            query: defaultQuery,
+            query: deferQuery,
             client: new ApolloClient({
                 link: new SubscriptionClient('ws://localhost:8084/', {
                         reconnect: true
@@ -38,10 +59,23 @@ export default class QueryRunner extends React.Component {
 
         this.handleChange = this.handleChange.bind(this);
         this.handleRun = this.handleRun.bind(this);
+        this.handleSampleQueryChange = this.handleSampleQueryChange.bind(this);
     }
 
     handleChange(event) {
         this.setState({ query: event.target.value });
+    }
+
+    handleSampleQueryChange(event) {
+        switch (event.target.value)
+        {
+            case "defer":
+                this.setState({ query: deferQuery });
+                break;
+            case "stream":
+                this.setState({ query: streamQuery });
+                break;
+        }
     }
 
     handleRun(event) {
@@ -65,10 +99,20 @@ export default class QueryRunner extends React.Component {
             <div>
                 <div>
                     <p>
-                        Type a query here. You can type subscriptions and defer directives, 
+                        Type a query here, or select one of the sample queries in the selection below. 
+                        You can type subscriptions and defer directives, 
                         the client will wait for more responses over Web Socket connection.
                     </p>
                     <p>Once typed, just click "Run query" to connect to the socket and wait for responses.</p>
+                </div>
+                <div>
+                    <p> 
+                        Sample queries:&nbsp;
+                        <select onChange={this.handleSampleQueryChange}>
+                            <option value="defer">Deferred query sample</option>
+                            <option value="stream">Streamed query sample</option>
+                        </select>
+                    </p>
                 </div>
                 <div>
                     <textarea cols="100" rows="20" value={this.state.query} onChange={this.handleChange}></textarea>
