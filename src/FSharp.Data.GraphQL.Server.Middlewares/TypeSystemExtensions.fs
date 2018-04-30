b8a9e4275ad1e55a52359f53537153dca1d45c6b
ivelten <ismaelcarlosvelten@gmail.com>
2018-04-30 18:23:05 -0300

    Implementing sort input field on field lists

--- src/FSharp.Data.GraphQL.Server.Middlewares/TypeSystemExtensions.fs
+++ src/FSharp.Data.GraphQL.Server.Middlewares/TypeSystemExtensions.fs
@@ -1,33 +1,64 @@
 namespace FSharp.Data.GraphQL.Server.Middlewares
 
 open FSharp.Data.GraphQL.Types
+open System.Reflection
+open Microsoft.FSharp.Reflection
 
 [<AutoOpen>]
 module TypeSystemExtensions =
     type Define with
-        static member OrderByInput = Define.Input("orderBy", ListOf String)
-        static member LastItemsInput = Define.Input("last", Int)
-        static member FirstItemsInput = Define.Input("first", Int)
-        static member SkipItemsInput = Define.Input("skip", Int)
+        static member SortInput = Define.Input("sort", ListOf String)
+        static member LastInput = Define.Input("last", Int)
+        static member FirstInput = Define.Input("first", Int)
+        static member SkipInput = Define.Input("skip", Int)
+        static member WhereInput = Define.Input("where", Obj)
         static member ListInputFields = 
-            [ Define.OrderByInput
-              Define.LastItemsInput
-              Define.FirstItemsInput
-              Define.SkipItemsInput ]
-        static member ListField(name : string, typedef : OutputDef<'Res>, description : string, resolve : ResolveFieldContext -> 'Val -> 'Res seq) : FieldDef<'Val> =
+            [ Define.SortInput
+              Define.LastInput
+              Define.FirstInput
+              Define.SkipInput
+              Define.WhereInput ]
+        static member ListField(name : string, typedef : #OutputDef<'Res>, description : string, 
+                                resolve : ResolveFieldContext -> 'Val -> 'Res seq,
+                                ?compileResolve : bool) : FieldDef<'Val> =
+            let getOrderByFuncs (fields : string seq) =
+                let getUnionValue value =
+                    let _, fields = FSharpValue.GetUnionFields(value, value.GetType())
+                    fields.[0]
+                let rec getPropertyValue (name : string) (r : obj) =
+                    let t = r.GetType()
+                    match t with
+                    | t when FSharpType.IsUnion t ->
+                        let unionValue = getUnionValue r 
+                        getPropertyValue name unionValue
+                    | t ->
+                        let flags =  BindingFlags.IgnoreCase ||| BindingFlags.Instance ||| BindingFlags.Public
+                        t.GetProperty(name, flags).GetValue(r) :?> System.IComparable
+                fields
+                |> Seq.map (fun field ->
+                    match field with
+                    | f when f.EndsWith("_ASC") && f.Length > 4 ->
+                        let f = f.Substring(0, f.Length - 4)
+                        fun (res : 'Res seq) -> res |> Seq.sortBy (fun res -> getPropertyValue f res)
+                    | f when f.EndsWith("_DESC") && f.Length > 5 ->
+                        let f = f.Substring(0, f.Length - 5)
+                        fun (res : 'Res seq) -> res |> Seq.sortByDescending (fun res -> getPropertyValue f res)
+                    | f -> fun (res : 'Res seq) -> res |> Seq.sortBy (fun res -> getPropertyValue f res))
             let resolve' = 
                 fun ctx v ->
                     let mutable res = resolve ctx v
                     ctx.Args
                     |> Map.iter (fun k v ->
-                        match k.ToLower() with
-                        | "skip" -> res <- res |> Seq.skip (v :?> int)
-                        | "first" -> res <- res |> Seq.take (v :?> int)
-                        | "last" -> res <- res |> Seq.rev |> Seq.take (v :?> int)
-                        | "orderBy" -> () // TODO: make the logic for filtering
+                        match k, v with
+                        | "skip", (:? int as index) -> res <- res |> Seq.skip index
+                        | "first", (:? int as index) -> res <- res |> Seq.take index
+                        | "last", (:? int as index) -> res <- res |> Seq.rev |> Seq.take index
+                        | "sort", (:? seq<string> as fields) -> getOrderByFuncs fields |> Seq.iter (fun f -> res <- f res)
                         | _ -> ())
                     res
-            Define.Field(name, ListOf typedef, description, Define.ListInputFields, resolve')
+            match defaultArg compileResolve true with
+            | true -> Define.Field(name, ListOf typedef, description, Define.ListInputFields, resolve')
+            | false -> Define.Field(name, ListOf typedef, description, Define.ListInputFields, resolve)
 
     type FieldDef<'Val> with
         member this.WithQueryWeight(weight : float) =
