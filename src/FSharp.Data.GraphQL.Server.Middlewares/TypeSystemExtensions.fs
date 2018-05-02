ff29bad00e15121f902bdd5ae93998cc3bca17d6
Ismael Carlos Velten <ismaelcarlosvelten@gmail.com>
2018-05-02 13:42:09 -0300

    Removing seq features for list fields

--- src/FSharp.Data.GraphQL.Server.Middlewares/TypeSystemExtensions.fs
+++ src/FSharp.Data.GraphQL.Server.Middlewares/TypeSystemExtensions.fs
@@ -1,30 +1,13 @@
 namespace FSharp.Data.GraphQL.Server.Middlewares
 
 open FSharp.Data.GraphQL.Types
-open FSharp.Data.GraphQL.Ast
 
 [<AutoOpen>]
 module TypeSystemExtensions =
     type Define with
         static member ListField(name : string, typedef : #OutputDef<'Res>, description : string, 
-                                resolve : ResolveFieldContext -> 'Val -> 'Res seq,
-                                ?compileResolve : bool) : FieldDef<'Val> =
-            let resolve' = 
-                fun ctx v ->
-                    let mutable res = resolve ctx v
-                    ctx.Args
-                    |> Map.iter (fun k v ->
-                        match k, v with
-                        | "skip", (:? int as index) -> res <- res |> Seq.skip index
-                        | "first", (:? int as index) -> res <- res |> Seq.take index
-                        | "last", (:? int as index) -> res <- res |> Seq.rev |> Seq.take index
-                        | "sort", (:? list<string> as fields) -> res <- res |> ListField.applySortFunc fields
-                        | "filter", (:? Value as args) -> res <- res |> ListField.applyFilterFunc args
-                        | _ -> ())
-                    res
-            match defaultArg compileResolve true with
-            | true -> Define.Field(name, ListOf typedef, description, ListField.inputs, resolve')
-            | false -> Define.Field(name, ListOf typedef, description, ListField.inputs, resolve)
+                                resolve : ResolveFieldContext -> 'Val -> 'Res seq) : FieldDef<'Val> =
+            Define.Field(name, ListOf typedef, description, ListField.inputs, resolve)
 
     type FieldDef<'Val> with
         member this.WithQueryWeight(weight : float) =
