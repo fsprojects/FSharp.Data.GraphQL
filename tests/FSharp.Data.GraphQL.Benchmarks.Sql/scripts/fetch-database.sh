a396c3999f8974905000d847153daa488f5c32e5
Ismael Carlos Velten <ismaelcarlosvelten@gmail.com>
2019-01-29 17:29:56 -0200

    Updating path in pre build script for linux

--- tests/FSharp.Data.GraphQL.Benchmarks.Sql/scripts/fetch-database.sh
+++ tests/FSharp.Data.GraphQL.Benchmarks.Sql/scripts/fetch-database.sh
@@ -1,12 +1,13 @@
-DB_PATH="$( cd "$(dirname $0)"; cd ..; pwd -P)"
-ZIP_FILE="${DB_PATH}/ml-latest.zip"
+PROJECT_PATH="$( cd "$(dirname $0)"; cd ..; pwd -P)"
+ZIP_FILE="${PROJECT_PATH}/ml-latest.zip"
+DB_PATH="${PROJECT_PATH}/ml-latest"
 
 echo "Checking if path \"${DB_PATH}\" exists..."
 
 if [ ! -d "${DB_PATH}" ]
 then
     echo "Path not found. Downloading database files..."
-    wget -P "${DB_PATH}" "http://files.grouplens.org/datasets/movielens/ml-latest.zip"
+    wget -P "${PROJECT_PATH}" "http://files.grouplens.org/datasets/movielens/ml-latest.zip"
     echo "Download finished. Unzipping database files..."
     unzip "$ZIP_FILE"
     echo "Unzipping done. Removing zip file..."
