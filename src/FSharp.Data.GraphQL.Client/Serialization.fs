ed00864e4c8061a0abb220c4c0bc29ea9b9a199e
Ismael Carlos Velten <ismaelcarlosvelten@gmail.com>
2019-02-26 13:43:31 -0300

    Changing JSON parser

--- src/FSharp.Data.GraphQL.Client/Serialization.fs
+++ src/FSharp.Data.GraphQL.Client/Serialization.fs
@@ -4,42 +4,123 @@
 module FSharp.Data.GraphQL.Client.Serialization
 
 open System
-open Newtonsoft.Json
-open Newtonsoft.Json.Serialization
+open FSharp.Data
 open Microsoft.FSharp.Reflection
+open System.Reflection
 
-type internal OptionConverter() =
-    inherit JsonConverter()
-    
-    override x.CanConvert(t) = 
-        t.IsGenericType && t.GetGenericTypeDefinition() = typedefof<option<_>>
+let private isOption (t : Type) = 
+    t.IsGenericType && t.GetGenericTypeDefinition() = typedefof<_ option>
 
-    override x.WriteJson(writer, value, serializer) =
-        let value = 
-            if value = null then null
-            else 
-                let _,fields = Microsoft.FSharp.Reflection.FSharpValue.GetUnionFields(value, value.GetType())
-                fields.[0]  
-        serializer.Serialize(writer, value)
+let private (|Option|_|) t =
+    if isOption t then Some (Option (t.GetGenericArguments().[0]))
+    else None
 
-    override x.ReadJson(reader, t, existingValue, serializer) = 
-        let innerType = t.GetGenericArguments().[0]
-        let innerType = 
-            if innerType.IsValueType then (typedefof<Nullable<_>>).MakeGenericType([|innerType|])
-            else innerType        
-        let value = serializer.Deserialize(reader, innerType)
-        let cases = FSharpType.GetUnionCases(t)
-        if value = null then FSharpValue.MakeUnion(cases.[0], [||])
-        else FSharpValue.MakeUnion(cases.[1], [|value|])
+let private (|Array|_|) (t : Type) =
+    if t.IsArray then Some (Array (t.GetElementType()))
+    else None
 
-let internal settings = 
-    JsonSerializerSettings(
-        Converters = [| OptionConverter() |],
-        ContractResolver = CamelCasePropertyNamesContractResolver())
+let private (|List|_|) (t : Type) =
+    if t.IsGenericTypeDefinition
+    then
+        let gtype = t.GetGenericTypeDefinition()
+        if gtype = typedefof<_ list>
+        then Some (List (gtype.GetGenericArguments().[0]))
+        else None
+    else None
 
-/// Serializes provided object to JSON format.
-let toJson (o: 't) : string = JsonConvert.SerializeObject(o, settings)
+let private (|Seq|_|) (t : Type) =
+    if t.IsGenericTypeDefinition
+    then
+        let gtype = t.GetGenericTypeDefinition()
+        if gtype = typedefof<seq<_>>
+        then Some (Seq (gtype.GetGenericArguments().[0]))
+        else None
+    else None
 
-/// Deserializes provided JSON string to object of given type.
-let fromJson<'t> (json: string) : 't = JsonConvert.DeserializeObject<'t>(json, settings)
+let private makeOption t values =
+    let otype = typedefof<_ option>
+    let cases = FSharpType.GetUnionCases(otype.MakeGenericType([|t|]))
+    FSharpValue.MakeUnion(cases.[0], values)
 
+let private downcastNone<'T> t : 'T =
+    match t with
+    | Option t -> downcast (makeOption t [||])
+    | _ -> failwithf "Error parsing JSON value: %O is not an option value." t
+
+let private downcastSome<'T> t value : 'T =
+    match t with
+    | Option t -> downcast (makeOption t [|value|])
+    | _ -> failwithf "Error parsing JSON value: %O is not an option value." t
+
+let private downcastType (t : Type) x : 'T = 
+    match t with
+    | Option t -> downcast (makeOption t [|x|])
+    | _ -> downcast (Convert.ChangeType(x, t))
+
+let private isNumericType (t : Type) =
+    [| typeof<decimal>
+       typeof<double>
+       typeof<single>
+       typeof<uint64>
+       typeof<int64>
+       typeof<uint32>
+       typeof<int>
+       typeof<uint16>
+       typeof<int16>
+       typeof<byte>
+       typeof<sbyte> |]
+    |> Array.exists (fun nt ->
+        match t with
+        | Option t -> nt = t
+        | _ -> nt = t)
+
+let private isStringType (t : Type) =
+    match t with
+    | Option t -> t = typeof<string>
+    | _ -> t = typeof<string>
+
+let private isBooleanType (t : Type) =
+    match t with
+    | Option t -> t = typeof<bool>
+    | _ -> t = typeof<bool>
+
+let private downcastNumber (t : Type) n =
+    match t with
+    | t when isNumericType t -> downcastType t n
+    | _ -> failwithf "Error parsing JSON value: %O is not a numeric type." t
+
+let private downcastString (t : Type) s =
+    match t with
+    | t when isStringType t -> downcastType t s
+    | _ -> failwithf "Error parsing JSON value: %O is not a string type." t
+
+let private downcastBoolean (t : Type) b =
+    match t with
+    | t when isBooleanType t -> downcastType t b
+    | _ -> failwithf "Error parsing JSON value: %O is not a boolean type." t
+
+let private getPropertyValue (t : Type) (converter : Type -> JsonValue -> 'T) (propName, propValue) =
+    let propType = t.GetProperty(propName, BindingFlags.Public ||| BindingFlags.Instance).PropertyType
+    converter propType propValue
+
+let private getArrayValue (t : Type) (converter : Type -> JsonValue -> 'TItem) (items : JsonValue []) : 'TArray =
+    match t with
+    | Array titem -> items |> Array.map (converter titem) |> downcastType t
+    | List titem -> items |> Array.map (converter titem) |> List.ofArray |> downcastType t
+    | Seq titem -> items |> Array.map (converter titem) |> Seq.ofArray |> downcastType t
+    | _ -> failwithf "Error parsing JSON value: %O is not an array type." t
+
+let fromJson<'T> (json : string) : 'T =
+    let t = typeof<'T>
+    let rec convert t parsed : 'T =
+        match parsed with
+        | JsonValue.Null -> downcastNone t
+        | JsonValue.String s -> downcastString t s
+        | JsonValue.Number n -> downcastNumber t n
+        | JsonValue.Float n -> downcastNumber t n
+        | JsonValue.Record props -> 
+            let vals = props |> Array.map (getPropertyValue t convert >> box)
+            Activator.CreateInstance(t, vals) |> downcastType t
+        | JsonValue.Array items -> items |> getArrayValue t convert
+        | JsonValue.Boolean b -> downcastBoolean t b
+    JsonValue.Parse(json) |> convert t
