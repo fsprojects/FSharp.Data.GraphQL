0b3c69bd4a0f71211fcea50a8efc007020b42b52
Ismael Carlos Velten <ismaelcarlosvelten@gmail.com>
2019-02-27 16:22:34 -0300

    Setting configurations for projects

--- tests/FSharp.Data.GraphQL.Tests/SerializationTests.fs
+++ tests/FSharp.Data.GraphQL.Tests/SerializationTests.fs
@@ -4,3 +4,34 @@
 
 module FSharp.Data.GraphQL.Tests.SerializationTests
 
+open System
+open FSharp.Data
+open FSharp.Data.GraphQL.Client
+
+type TestRecord =
+    { Prop1 : string
+      Prop2 : int }
+
+[<Fact>]
+let ``serialize should correctly serialize basic types`` () =
+    let input = [| 
+        box 1y
+        upcast 2uy
+        upcast 3s
+        upcast 4us
+        upcast 5
+        upcast 6u
+        upcast 7l
+        upcast 8ul
+        upcast 9.0f
+        upcast 10.0
+        upcast 11.0m
+        upcast "test"
+        upcast Guid.Parse("e433c15b-f435-4574-bcbf-a1e35b73fff7")
+        null
+        upcast { Prop1 = "test"; Prop2 = 1 } |]
+    let actual = Serialization.serialize input
+    let expected =
+        JsonValue.Array [|
+            JsonValue.Number 1m |]
+    actual |> seqEquals expected
