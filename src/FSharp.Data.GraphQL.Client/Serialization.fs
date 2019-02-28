404d10e83d971826465342d9b58e5b2bb8227274
Ismael Carlos Velten <ismaelcarlosvelten@gmail.com>
2019-02-28 13:08:46 -0300

    Improving serialization tests

--- src/FSharp.Data.GraphQL.Client/Serialization.fs
+++ src/FSharp.Data.GraphQL.Client/Serialization.fs
@@ -5,11 +5,15 @@ module FSharp.Data.GraphQL.Client.Serialization
 
 open System
 open FSharp.Data
-open FSharp.Data.JsonExtensions
 open Microsoft.FSharp.Reflection
 open System.Reflection
 open System.Collections
 open System.Collections.Generic
+open System.Globalization
+
+let private isoDateFormat = "yyyy-MM-dd" 
+let private isoDateTimeFormat = "O"
+let private isoFormats = [|isoDateTimeFormat; isoDateFormat|]
 
 let private isOption (t : Type) = 
     t.IsGenericType && t.GetGenericTypeDefinition() = typedefof<_ option>
@@ -23,42 +27,44 @@ let private (|Array|_|) (t : Type) =
     else None
 
 let private (|List|_|) (t : Type) =
-    if t.IsGenericTypeDefinition
+    if t.IsGenericType
     then
         let gtype = t.GetGenericTypeDefinition()
         if gtype = typedefof<_ list>
-        then Some (List (gtype.GetGenericArguments().[0]))
+        then Some (List (t.GetGenericArguments().[0]))
         else None
     else None
 
 let private (|Seq|_|) (t : Type) =
-    if t.IsGenericTypeDefinition
+    if t.IsGenericType
     then
         let gtype = t.GetGenericTypeDefinition()
         if gtype = typedefof<seq<_>>
-        then Some (Seq (gtype.GetGenericArguments().[0]))
+        then Some (Seq (t.GetGenericArguments().[0]))
         else None
     else None
 
-let private makeOption t values =
+let private makeOption t (value : obj) =
     let otype = typedefof<_ option>
     let cases = FSharpType.GetUnionCases(otype.MakeGenericType([|t|]))
-    FSharpValue.MakeUnion(cases.[0], values)
+    match value with
+    | null -> FSharpValue.MakeUnion(cases.[0], [||])
+    | _ -> FSharpValue.MakeUnion(cases.[1], [|value|])
 
-let private downcastNone<'T> t : 'T =
+let private downcastNone<'T> t =
     match t with
-    | Option t -> downcast (makeOption t [||])
+    | Option t -> downcast (makeOption t null)
     | _ -> failwithf "Error parsing JSON value: %O is not an option value." t
 
-let private downcastSome<'T> t value : 'T =
+let private downcastType (t : Type) x = 
     match t with
-    | Option t -> downcast (makeOption t [|value|])
-    | _ -> failwithf "Error parsing JSON value: %O is not an option value." t
-
-let private downcastType (t : Type) x : 'T = 
-    match t with
-    | Option t -> downcast (makeOption t [|x|])
+    | Option t -> downcast (makeOption t (Convert.ChangeType(x, t)))
     | _ -> downcast (Convert.ChangeType(x, t))
+
+let private isType (expected : Type) (t : Type) =
+    match t with
+    | Option t -> t = expected
+    | _ -> t = expected
 
 let private isNumericType (t : Type) =
     [| typeof<decimal>
@@ -72,29 +78,34 @@ let private isNumericType (t : Type) =
        typeof<int16>
        typeof<byte>
        typeof<sbyte> |]
-    |> Array.exists (fun nt ->
-        match t with
-        | Option t -> nt = t
-        | _ -> nt = t)
+    |> Array.exists (fun expected -> isType expected t)
 
-let private isStringType (t : Type) =
-    match t with
-    | Option t -> t = typeof<string>
-    | _ -> t = typeof<string>
-
-let private isBooleanType (t : Type) =
-    match t with
-    | Option t -> t = typeof<bool>
-    | _ -> t = typeof<bool>
+let private isStringType = isType typeof<string>
+let private isDateTimeType = isType typeof<DateTime>
+let private isDateTimeOffsetType = isType typeof<DateTimeOffset>
+let private isGuidType = isType typeof<Guid>
+let private isBooleanType = isType typeof<bool>
 
 let private downcastNumber (t : Type) n =
     match t with
     | t when isNumericType t -> downcastType t n
     | _ -> failwithf "Error parsing JSON value: %O is not a numeric type." t
 
-let private downcastString (t : Type) s =
+let private downcastString (t : Type) (s : string) =
     match t with
     | t when isStringType t -> downcastType t s
+    | t when isDateTimeType t ->
+        match DateTime.TryParseExact(s, isoFormats, CultureInfo.InvariantCulture, DateTimeStyles.None) with
+        | (true, d) -> downcastType t d
+        | _ -> failwithf "Error parsing JSON value: %O is a date type, but parsing of value \"%s\" failed." t s
+    | t when isDateTimeOffsetType t ->
+        match DateTimeOffset.TryParseExact(s, isoFormats, CultureInfo.InvariantCulture, DateTimeStyles.None) with
+        | (true, d) -> downcastType t d
+        | _ -> failwithf "Error parsing JSON value: %O is a date time offset type, but parsing of value \"%s\" failed." t s
+    | t when isGuidType t ->
+        match Guid.TryParse(s) with
+        | (true, g) -> downcastType t g
+        | _ -> failwithf "Error parsing JSON value: %O is a Guid type, but parsing of value \"%s\" failed." t s
     | _ -> failwithf "Error parsing JSON value: %O is not a string type." t
 
 let private downcastBoolean (t : Type) b =
@@ -106,41 +117,60 @@ let private getPropertyValue (t : Type) (converter : Type -> JsonValue -> 'T) (p
     let propType = t.GetProperty(propName, BindingFlags.Public ||| BindingFlags.Instance).PropertyType
     converter propType propValue
 
-let private getArrayValue (t : Type) (converter : Type -> JsonValue -> 'TItem) (items : JsonValue []) : 'TArray =
+let rec private getArrayValue (t : Type) (converter : Type -> JsonValue -> obj) (items : JsonValue []) =
+    let castArray itemType (items : obj []) : obj =
+        let arr = Array.CreateInstance(itemType, items.Length)
+        items |> Array.iteri (fun ix i -> arr.SetValue(i, ix))
+        upcast arr
+    let castList itemType (items : obj list) =
+        let tlist = typedefof<_ list>.MakeGenericType([|itemType|])
+        let empty = 
+            let uc = 
+                Reflection.FSharpType.GetUnionCases(tlist) 
+                |> Seq.filter (fun uc -> uc.Name = "Empty") 
+                |> Seq.exactlyOne
+            Reflection.FSharpValue.MakeUnion(uc, [||])
+        let rec helper items =
+            match items with
+            | [] -> empty
+            | [x] -> Activator.CreateInstance(tlist, [|x; empty|])
+            | x :: xs -> Activator.CreateInstance(tlist, [|x; helper xs|])
+        helper items
     match t with
-    | Array titem -> items |> Array.map (converter titem) |> downcastType t
-    | List titem -> items |> Array.map (converter titem) |> List.ofArray |> downcastType t
-    | Seq titem -> items |> Array.map (converter titem) |> Seq.ofArray |> downcastType t
+    | Option t -> getArrayValue t converter items |> makeOption t
+    | Array itype | Seq itype -> items |> Array.map (converter itype) |> castArray itype
+    | List itype -> items |> Array.map (converter itype) |> Array.toList |> castList itype
     | _ -> failwithf "Error parsing JSON value: %O is not an array type." t
+
+let rec private convert t parsed : obj =
+    match parsed with
+    | JsonValue.Null -> downcastNone t
+    | JsonValue.String s -> downcastString t s
+    | JsonValue.Number n -> downcastNumber t n
+    | JsonValue.Float n -> downcastNumber t n
+    | JsonValue.Record props -> 
+        let vals = props |> Array.map (getPropertyValue t convert >> box)
+        Activator.CreateInstance(t, vals) |> downcastType t
+    | JsonValue.Array items -> items |> getArrayValue t convert
+    | JsonValue.Boolean b -> downcastBoolean t b
 
 let deserializeRecord<'T> (json : string) : 'T =
     let t = typeof<'T>
-    let rec convert t parsed : 'T =
-        match parsed with
-        | JsonValue.Null -> downcastNone t
-        | JsonValue.String s -> downcastString t s
-        | JsonValue.Number n -> downcastNumber t n
-        | JsonValue.Float n -> downcastNumber t n
-        | JsonValue.Record props -> 
-            let vals = props |> Array.map (getPropertyValue t convert >> box)
-            Activator.CreateInstance(t, vals) |> downcastType t
-        | JsonValue.Array items -> items |> getArrayValue t convert
-        | JsonValue.Boolean b -> downcastBoolean t b
-    JsonValue.Parse(json) |> convert t
+    downcast (JsonValue.Parse(json) |> convert t)
 
-let deserializeDict (json : string) : IDictionary<string, obj> =
-    let rec convert parsed : obj =
-        match parsed with
-        | JsonValue.Null -> null
-        | JsonValue.String s -> upcast s
-        | JsonValue.Number n -> upcast n
-        | JsonValue.Float n -> upcast n
-        | JsonValue.Record props -> upcast (props |> Array.map (fun (n, v) -> (n, (convert v))) |> dict)
-        | JsonValue.Array items -> upcast (items |> Array.map convert)
-        | JsonValue.Boolean b -> upcast b
-    match JsonValue.Parse(json) with
-    | JsonValue.Record props -> props |> Array.map (fun (n, v) -> (n, (convert v))) |> dict
-    | _ -> failwith "The input JSON could not be deserialized to a record type."
+let (|EnumerableValue|_|) (x : obj) =
+    match x with
+    | :? IEnumerable as x -> Some (EnumerableValue (Seq.cast<obj> x |> Array.ofSeq))
+    | _ -> None
+
+let (|OptionValue|_|) (x : obj) =
+    let xtype = x.GetType()
+    if isOption xtype
+    then
+        match FSharpValue.GetUnionFields(x, xtype) with
+        | (_, [|value|]) -> Some (OptionValue Some value)
+        | _ -> Some (OptionValue None)
+    else None
 
 let serialize (x : obj) =
     let rec helper (x : obj) : JsonValue =
@@ -158,16 +188,14 @@ let serialize (x : obj) =
         | :? decimal as x -> JsonValue.Float (float x)
         | :? string as x -> JsonValue.String x
         | :? Guid as x -> JsonValue.String (x.ToString())
-        | :? DateTime as x when x.Date = x -> JsonValue.String (x.ToString("yyyy-MM-dd"))
-        | :? DateTime as x -> JsonValue.String (x.ToString("O"))
-        | :? DateTimeOffset as x -> JsonValue.String (x.ToString("O"))
-        | :? IEnumerable as x -> 
-            Seq.cast<obj> x 
-            |> Array.ofSeq 
-            |> Array.map helper
-            |> JsonValue.Array
+        | :? DateTime as x when x.Date = x -> JsonValue.String (x.ToString(isoDateFormat))
+        | :? DateTime as x -> JsonValue.String (x.ToString(isoDateTimeFormat))
+        | :? DateTimeOffset as x -> JsonValue.String (x.ToString(isoDateTimeFormat))
         | :? bool as x -> JsonValue.Boolean x
+        | EnumerableValue items -> items |> Array.map helper |> JsonValue.Array
         | null -> JsonValue.Null
+        | OptionValue None -> JsonValue.Null
+        | OptionValue (Some x) -> helper x
         | _ ->
             let xtype = x.GetType()
             let xprops = xtype.GetProperties(BindingFlags.Public ||| BindingFlags.Instance)
@@ -175,3 +203,17 @@ let serialize (x : obj) =
             JsonValue.Record items
     let value = helper x
     value.ToString()
+
+let deserializeDict (json : string) : IDictionary<string, obj> =
+    let rec convert parsed : obj =
+        match parsed with
+        | JsonValue.Null -> null
+        | JsonValue.String s -> upcast s
+        | JsonValue.Number n -> upcast n
+        | JsonValue.Float n -> upcast n
+        | JsonValue.Record props -> upcast (props |> Array.map (fun (n, v) -> (n, (convert v))) |> dict)
+        | JsonValue.Array items -> upcast (items |> Array.map convert)
+        | JsonValue.Boolean b -> upcast b
+    match JsonValue.Parse(json) with
+    | JsonValue.Record props -> props |> Array.map (fun (n, v) -> (n, (convert v))) |> dict
+    | _ -> failwith "The input JSON could not be deserialized to a record type."
--- tests/FSharp.Data.GraphQL.Tests/SerializationTests.fs
+++ tests/FSharp.Data.GraphQL.Tests/SerializationTests.fs
@@ -9,56 +9,197 @@ open System
 open FSharp.Data
 open FSharp.Data.GraphQL.Client
 
-type TestRecord =
-    { Prop1 : string
-      Prop2 : int }
-
 let private normalize (json : string) =
   json.Replace("\r\n", "\n")
 
+type TestRecord1 =
+  { Prop1 : sbyte
+    Prop2 : byte
+    Prop3 : int16
+    Prop4 : uint16
+    Prop5 : int
+    Prop6 : uint32
+    Prop7 : int64
+    Prop8 : uint64
+    Prop9 : single
+    Prop10 : double
+    Prop11 : decimal
+    Prop12 : DateTime
+    Prop13 : DateTime
+    Prop14 : DateTimeOffset
+    Prop15 : string
+    Prop16 : Guid
+    Prop17 : string option
+    Prop18 : int []
+    Prop19 : int list
+    Prop20 : seq<int> }
+
+type TestRecord2 =
+  { Prop1 : sbyte option
+    Prop2 : byte option
+    Prop3 : int16 option
+    Prop4 : uint16 option
+    Prop5 : int option
+    Prop6 : uint32 option
+    Prop7 : int64 option
+    Prop8 : uint64 option
+    Prop9 : single option
+    Prop10 : double option
+    Prop11 : decimal option
+    Prop12 : DateTime option
+    Prop13 : DateTime option
+    Prop14 : DateTimeOffset option
+    Prop15 : string option
+    Prop16 : Guid option
+    Prop17 : string option
+    Prop18 : int [] option
+    Prop19 : int list option
+    Prop20 : seq<int> option }
+
+let subject1 =
+    { TestRecord1.Prop1 = 1y
+      Prop2 = 2uy
+      Prop3 = 3s
+      Prop4 = 4us
+      Prop5 = 5
+      Prop6 = 6u
+      Prop7 = 7L
+      Prop8 = 8UL
+      Prop9 = 9.0f
+      Prop10 = 10.0
+      Prop11 = 11.0m
+      Prop12 = DateTime(2016, 1, 1)
+      Prop13 = DateTime(2016, 1, 1, 10, 50, 25, 326)
+      Prop14 = DateTimeOffset(DateTime(2016, 1, 1, 10, 50, 25, 326))
+      Prop15 = "test"
+      Prop16 = Guid.Parse("e433c15b-f435-4574-bcbf-a1e35b73fff7")
+      Prop17 = None
+      Prop18 = [| 1; 2; 3 |]
+      Prop19 = [ 1; 2; 3 ]
+      Prop20 = [| 1; 2; 3 |] }
+
+let subject2 =
+    { Prop1 = Some 1y
+      Prop2 = Some 2uy
+      Prop3 = Some 3s
+      Prop4 = Some 4us
+      Prop5 = Some 5
+      Prop6 = Some 6u
+      Prop7 = Some 7L
+      Prop8 = Some 8UL
+      Prop9 = Some 9.0f
+      Prop10 = Some 10.0
+      Prop11 = Some 11.0m
+      Prop12 = Some (DateTime(2016, 1, 1))
+      Prop13 = Some (DateTime(2016, 1, 1, 10, 50, 25, 326))
+      Prop14 = Some (DateTimeOffset(DateTime(2016, 1, 1, 10, 50, 25, 326)))
+      Prop15 = Some "test"
+      Prop16 = Some (Guid.Parse("e433c15b-f435-4574-bcbf-a1e35b73fff7"))
+      Prop17 = None
+      Prop18 = Some [| 1; 2; 3 |]
+      Prop19 = Some [ 1; 2; 3 ]
+      Prop20 = Some (upcast [| 1; 2; 3 |]) }
+
+let subject3 =
+    { Prop1 = None
+      Prop2 = None
+      Prop3 = None
+      Prop4 = None
+      Prop5 = None
+      Prop6 = None
+      Prop7 = None
+      Prop8 = None
+      Prop9 = None
+      Prop10 = None
+      Prop11 = None
+      Prop12 = None
+      Prop13 = None
+      Prop14 = None
+      Prop15 = None
+      Prop16 = None
+      Prop17 = None
+      Prop18 = None
+      Prop19 = None
+      Prop20 = None }
+
+let json = normalize """{
+  "Prop1": 1,
+  "Prop2": 2,
+  "Prop3": 3,
+  "Prop4": 4,
+  "Prop5": 5,
+  "Prop6": 6,
+  "Prop7": 7,
+  "Prop8": 8,
+  "Prop9": 9,
+  "Prop10": 10,
+  "Prop11": 11,
+  "Prop12": "2016-01-01",
+  "Prop13": "2016-01-01T10:50:25.3260000",
+  "Prop14": "2016-01-01T10:50:25.3260000-02:00",
+  "Prop15": "test",
+  "Prop16": "e433c15b-f435-4574-bcbf-a1e35b73fff7",
+  "Prop17": null,
+  "Prop18": [
+    1,
+    2,
+    3
+  ],
+  "Prop19": [
+    1,
+    2,
+    3
+  ],
+  "Prop20": [
+    1,
+    2,
+    3
+  ]
+}"""
+
+let nullJson = normalize """{
+  "Prop1": null,
+  "Prop2": null,
+  "Prop3": null,
+  "Prop4": null,
+  "Prop5": null,
+  "Prop6": null,
+  "Prop7": null,
+  "Prop8": null,
+  "Prop9": null,
+  "Prop10": null,
+  "Prop11": null,
+  "Prop12": null,
+  "Prop13": null,
+  "Prop14": null,
+  "Prop15": null,
+  "Prop16": null,
+  "Prop17": null,
+  "Prop18": null,
+  "Prop19": null,
+  "Prop20": null
+}"""
+
+[<Fact>]
+let ``deserializeRecord should correctly deserialize basic types`` () =
+  let actual = Serialization.deserializeRecord<TestRecord1> json
+  actual |> equals subject1
+
+[<Fact>]
+let ``deserializeRecord should correctly deserialize option types`` () =
+  let actual = [|
+    Serialization.deserializeRecord<TestRecord2> json
+    Serialization.deserializeRecord<TestRecord2> nullJson |]
+  actual |> seqEquals [|subject2; subject3|]
+
 [<Fact>]
 let ``serialize should correctly serialize basic types`` () =
-    let input = [| 
-        box 1y
-        upcast 2uy
-        upcast 3s
-        upcast 4us
-        upcast 5
-        upcast 6u
-        upcast 7l
-        upcast 8ul
-        upcast 9.0f
-        upcast 10.0
-        upcast 11.0m
-        upcast DateTime(2016, 1, 1)
-        upcast DateTime(2016, 1, 1, 10, 50, 25, 326)
-        upcast DateTimeOffset(DateTime(2016, 1, 1, 10, 50, 25, 326))
-        upcast "test"
-        upcast Guid.Parse("e433c15b-f435-4574-bcbf-a1e35b73fff7")
-        null
-        upcast { Prop1 = "test"; Prop2 = 1 } |]
-    let actual = Serialization.serialize input |> normalize
-    let expected = normalize """[
-  1,
-  2,
-  3,
-  4,
-  5,
-  6,
-  7,
-  8,
-  9,
-  10,
-  11,
-  "2016-01-01",
-  "2016-01-01T10:50:25.3260000",
-  "2016-01-01T10:50:25.3260000-02:00",
-  "test",
-  "e433c15b-f435-4574-bcbf-a1e35b73fff7",
-  null,
-  {
-    "Prop1": "test",
-    "Prop2": 1
-  }
-]"""
-    actual |> equals expected
+    let actual = Serialization.serialize subject1 |> normalize
+    actual |> equals json
+
+[<Fact>]
+let ``serialize should correctly serialize option types`` () =
+    let actual = [|
+      Serialization.serialize subject2 |> normalize
+      Serialization.serialize subject3 |> normalize |]
+    actual |> seqEquals [|json; nullJson|]
