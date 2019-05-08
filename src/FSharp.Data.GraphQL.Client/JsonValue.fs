fa59395a8d579cddf0b8696a454e51c895568070
John Berzy <john.berzy@mybazinga.com>
2019-05-07 23:22:27 -0700

    Update release notes and copyright

--- src/FSharp.Data.GraphQL.Client/JsonValue.fs
+++ src/FSharp.Data.GraphQL.Client/JsonValue.fs
@@ -1,4 +1,16 @@
-﻿namespace FSharp.Data.GraphQL.Client
+﻿// --------------------------------------------------------------------------------------
+// Copyright (c) Microsoft Corporation 2005-2012.
+// This sample code is provided "as is" without warranty of any kind.
+// We disclaim all warranties, either express or implied, including the
+// warranties of merchantability and fitness for a particular purpose.
+//
+// A simple F# portable parser for JSON data
+// --------------------------------------------------------------------------------------
+
+/// The MIT License (MIT)
+/// Copyright (c) 2016 Bazinga Technologies Inc
+
+namespace FSharp.Data.GraphQL.Client
 
 open System
 open System.IO
--- src/FSharp.Data.GraphQL.Client/TextConversions.fs
+++ src/FSharp.Data.GraphQL.Client/TextConversions.fs
@@ -1,4 +1,13 @@
-﻿namespace FSharp.Data.GraphQL.Client
+﻿// --------------------------------------------------------------------------------------
+// Copyright (c) Microsoft Corporation 2005-2012.
+// This sample code is provided "as is" without warranty of any kind.
+// We disclaim all warranties, either express or implied, including the
+// warranties of merchantability and fitness for a particular purpose.
+//
+// A simple F# portable parser for JSON data
+// --------------------------------------------------------------------------------------
+
+namespace FSharp.Data.GraphQL.Client
 
 open System
 open System.Globalization
