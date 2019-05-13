566aa5abf98c2ffd476c8d3158f3f135e30eb181
Ismael Carlos Velten <ismaelcarlosvelten@gmail.com>
2019-05-13 12:56:01 -0300

    Updating assembly info for the latest version

--- src/FSharp.Data.GraphQL.Server.Middlewares/AssemblyInfo.fs
+++ src/FSharp.Data.GraphQL.Server.Middlewares/AssemblyInfo.fs
@@ -5,13 +5,13 @@ open System.Reflection
 [<assembly: AssemblyTitleAttribute("FSharp.Data.GraphQL.Server.Middlewares")>]
 [<assembly: AssemblyProductAttribute("FSharp.Data.GraphQL")>]
 [<assembly: AssemblyDescriptionAttribute("FSharp implementation of Facebook GraphQL query language")>]
-[<assembly: AssemblyVersionAttribute("0.0.18")>]
-[<assembly: AssemblyFileVersionAttribute("0.0.18")>]
+[<assembly: AssemblyVersionAttribute("1.0.0")>]
+[<assembly: AssemblyFileVersionAttribute("1.0.0")>]
 do ()
 
 module internal AssemblyVersionInformation =
     let [<Literal>] AssemblyTitle = "FSharp.Data.GraphQL.Server.Middlewares"
     let [<Literal>] AssemblyProduct = "FSharp.Data.GraphQL"
     let [<Literal>] AssemblyDescription = "FSharp implementation of Facebook GraphQL query language"
-    let [<Literal>] AssemblyVersion = "0.0.18"
-    let [<Literal>] AssemblyFileVersion = "0.0.18"
+    let [<Literal>] AssemblyVersion = "1.0.0"
+    let [<Literal>] AssemblyFileVersion = "1.0.0"
