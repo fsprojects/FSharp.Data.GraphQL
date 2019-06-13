2f5054f8e43ff0e638c43c14d2465920118ae9a3
Ismael Carlos Velten <ismaelcarlosvelten@gmail.com>
2019-06-13 17:07:13 -0300

    Validation of subscriptions single root field

--- src/FSharp.Data.GraphQL.Shared/Validation.fs
+++ src/FSharp.Data.GraphQL.Shared/Validation.fs
@@ -6,6 +6,7 @@ module FSharp.Data.GraphQL.Validation
 open FSharp.Data.GraphQL.Types
 open FSharp.Data.GraphQL.Types.Patterns
 open FSharp.Data.GraphQL.Ast
+open FSharp.Data.GraphQL.Types.Introspection
 
 type ValidationResult =
     | Success
@@ -62,16 +63,56 @@ module Types =
         |> Seq.fold (fun acc namedDef -> acc @ validateType namedDef) Success
 
 module Ast =
-    let validateOperationNameUniqueness (operations : OperationDefinition list) =
-        let names = operations |> List.choose (fun x -> x.Name)
+    let private getOperationDefinitions (ast : Document) =
+        ast.Definitions |> List.choose (function | OperationDefinition x -> Some x | _ -> None)
+
+    let private getFragmentDefinitions (ast : Document) =
+        ast.Definitions |> List.choose (function | FragmentDefinition x -> Some x | _ -> None)
+
+    let validateOperationNameUniqueness (ast : Document) =
+        let names = getOperationDefinitions ast |> List.choose (fun x -> x.Name)
         names
         |> List.map (fun name -> name, names |> List.filter (fun x -> x = name) |> List.length)
         |> List.distinctBy fst
         |> List.filter (fun (_, count) -> count > 0)
         |> List.fold (fun acc (name, count) -> acc @ Error [ (sprintf "Operation '%s' has %i definitions. Each operation name must be unique." name count ) ]) Success
 
-    let validateLoneAnonymousOperation (operations : OperationDefinition list) =
+    let validateLoneAnonymousOperation (ast : Document) =
+        let operations = getOperationDefinitions ast
         let unamed = operations |> List.filter (fun x -> x.Name.IsNone)
         if unamed.Length = 1 && operations.Length = 1
         then Success
         else Error [ "Anonymous operations must be the only operation in a document. This document has at least one anonymous operation and more than one operation." ]
+
+    let validateSubscriptionSingleRootField (ast : Document) =
+        let fragmentDefinitions = getFragmentDefinitions ast
+        let rec getFieldNames (acc : string list) (selectionSet : Selection list) =
+            match selectionSet with
+            | [] -> acc
+            | selection :: tail ->
+                let acc =
+                    match selection with
+                    | Field field -> field.Name :: acc
+                    | InlineFragment frag -> getFieldNames acc frag.SelectionSet
+                    | FragmentSpread spread ->
+                        let frag = fragmentDefinitions |> List.tryFind (fun x -> x.Name.IsSome && x.Name.Value = spread.Name)
+                        match frag with
+                        | Some frag -> getFieldNames acc frag.SelectionSet
+                        | None -> acc
+                getFieldNames acc tail
+        let rec validate (acc : ValidationResult) (operationName : string option) (fieldNames : string list) =
+            if fieldNames.Length = 1
+            then acc
+            else
+                let rec fieldNamesToString (acc : string) (fieldNames : string list) =
+                    match fieldNames with
+                    | [] -> acc
+                    | [last] -> acc + " " + last
+                    | fieldName :: tail -> fieldNamesToString (acc + fieldName + ", ") tail
+                let fieldNamesAsString = fieldNamesToString "" fieldNames
+                match operationName with
+                | Some operationName -> acc @ Error [ sprintf "Subscription operations should have only one root field. Operation '%s' has %i fields (%s)." operationName fieldNames.Length fieldNamesAsString ]
+                | None -> acc @ Error [ sprintf "Subscription operations should have only one root field. Operation has %i fields (%s)." fieldNames.Length fieldNamesAsString ]
+        getOperationDefinitions ast
+        |> List.choose (fun def -> if def.OperationType = Subscription then Some (def.Name, getFieldNames [] def.SelectionSet) else None)
+        |> List.fold (fun acc (name, fieldNames) -> validate acc name fieldNames) Success
--- tests/FSharp.Data.GraphQL.Tests/AstValidationTests.fs
+++ tests/FSharp.Data.GraphQL.Tests/AstValidationTests.fs
@@ -5,12 +5,6 @@ module FSharp.Data.GraphQL.Tests.AstValidationTests
 open FSharp.Data.GraphQL
 open FSharp.Data.GraphQL.Validation
 open Xunit
-open FSharp.Data.GraphQL.Ast
-
-let parseOperationDefinitions (query : string) =
-    let ast = Parser.parse query
-    ast.Definitions
-    |> List.choose (function | OperationDefinition x -> Some x | _ -> None)
 
 [<Fact>]
 let ``Validation should grant that each operation name is unique in the document`` () =
@@ -26,7 +20,7 @@ mutation dogOperation {
     id
   }
 }"""
-    let actual = parseOperationDefinitions query |> Validation.Ast.validateOperationNameUniqueness
+    let actual = Parser.parse query |> Validation.Ast.validateOperationNameUniqueness
     let expected = Error [ "Operation 'dogOperation' has 2 definitions. Each operation name must be unique." ]
     actual |> equals expected
 
@@ -46,6 +40,43 @@ query getName {
     }
   }
 }"""
-    let actual = parseOperationDefinitions query |> Validation.Ast.validateLoneAnonymousOperation
+    let actual = Parser.parse query |> Validation.Ast.validateLoneAnonymousOperation
     let expected = Error [ "Anonymous operations must be the only operation in a document. This document has at least one anonymous operation and more than one operation." ]
+    actual |> equals expected
+
+[<Fact>]
+let ``Validation should grant that subscription operations contains only one root field`` () =
+    let query1 =
+        """subscription sub1 {
+  newMessage {
+    body
+    sender
+  }
+  disallowedSecondRootField
+}"""
+    let query2 =
+        """subscription sub2 {
+  ...multipleSubscriptions
+}
+
+fragment multipleSubscriptions on Subscription {
+  newMessage {
+    body
+    sender
+  }
+  disallowedSecondRootField
+}"""
+    let query3 =
+        """subscription sub3 {
+  newMessage {
+    body
+    sender
+  }
+  __typename
+}"""
+    let expected =
+        Error [ "Subscription operations should have only one root field. Operation 'sub1' has 2 fields (disallowedSecondRootField,  newMessage)."
+                "Subscription operations should have only one root field. Operation 'sub2' has 2 fields (disallowedSecondRootField,  newMessage)."
+                "Subscription operations should have only one root field. Operation 'sub3' has 2 fields (__typename,  newMessage)." ]
+    let actual = [query1; query2; query3] |> List.map (Parser.parse >> Validation.Ast.validateSubscriptionSingleRootField) |> List.reduce (@)
     actual |> equals expected
