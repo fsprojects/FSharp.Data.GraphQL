bfdbf9e8742cf5f0a53df9f456c3ccfa925e9b95
Ismael Carlos Velten <ismaelcarlosvelten@gmail.com>
2020-04-21 14:33:21 -0300

    Adding xml docs on constructors

--- src/FSharp.Data.GraphQL.Client/GraphQLClientConnection.fs
+++ src/FSharp.Data.GraphQL.Client/GraphQLClientConnection.fs
@@ -9,8 +9,12 @@ open System.Net.Http
 /// The connection component for GraphQL client operations.
 [<AllowNullLiteral>]
 type GraphQLClientConnection(client : HttpClient, ownsClient : bool) =
+    /// Creates a new instance of GraphQLClientConnection, using a provided HttpClient to be wrapped inside.
+    /// The provided HttpClient is not owned by the connection, so it must be disposed when it is not necessary anymore.
     new(client) = new GraphQLClientConnection(client, false)
 
+    /// Creates a new instance of GraphQLClientConnection, using a fresh new instance of HttpClient internally.
+    /// This instance is owned by the GraphQLClientConnection, being disposed when the connection itself is disposed.
     new() = new GraphQLClientConnection(new HttpClient(), true)
 
     member internal __.Client = client
