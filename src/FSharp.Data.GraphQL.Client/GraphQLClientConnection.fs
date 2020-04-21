e98069a3afe300bd2f2801ce0abd1050e1b32c70
Ismael Carlos Velten <ismaelcarlosvelten@gmail.com>
2020-04-21 18:30:42 -0300

    Updating connection component to accept HttpClient directly in another constructor

--- src/FSharp.Data.GraphQL.Client/GraphQLClientConnection.fs
+++ src/FSharp.Data.GraphQL.Client/GraphQLClientConnection.fs
@@ -9,15 +9,22 @@ open System.Net.Http
 /// The connection component for GraphQL client operations.
 [<AllowNullLiteral>]
 type GraphQLClientConnection(invoker : HttpMessageInvoker, ownsInvoker : bool) =
+    do if isNull invoker then raise <| ArgumentNullException("invoker")
+
     /// Creates a new instance of GraphQLClientConnection, using a provided System.Net.Http.HttpMessageInvoker to be wrapped inside.
     /// The provided System.Net.Http.HttpMessageInvoker is not owned by the connection, so it must be disposed when it is not necessary anymore.
     new(invoker) = new GraphQLClientConnection(invoker, false)
+
+    /// Creates a new instance of GraphQLClientConnection, using a provided System.Net.Http.HttpClient as a System.Net.Http.HttpMessageInvoker to be wrapped inside.
+    /// The provided System.Net.Http.HttpMessageInvoker is not owned by the connection, so it must be disposed when it is not necessary anymore.
+    new(invoker : HttpClient) = new GraphQLClientConnection(invoker, false)
 
     /// Creates a new instance of GraphQLClientConnection, using a fresh new instance of System.Net.Http.HttpClient as an System.Net.Http.HttpMessageInvoker.
     /// This instance is owned by the GraphQLClientConnection, being disposed when the connection itself is disposed.
     new() = new GraphQLClientConnection(new HttpClient(), true)
 
-    member internal __.Invoker = invoker
+    /// The System.Net.Http.HttpMessageInvoker used by this connection.
+    member __.Invoker = invoker
     
     interface IDisposable with
         member __.Dispose() = if ownsInvoker then invoker.Dispose()
