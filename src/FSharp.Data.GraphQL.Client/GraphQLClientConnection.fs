bee316e617eeb68def920e4c1d44b468e406a00f
Ismael Carlos Velten <ismaelcarlosvelten@gmail.com>
2020-12-01 11:02:14 -0300

    Adding an overload for GraphQLClientConnection to use IHttpClientFactory as dependency

--- src/FSharp.Data.GraphQL.Client/GraphQLClientConnection.fs
+++ src/FSharp.Data.GraphQL.Client/GraphQLClientConnection.fs
@@ -15,12 +15,16 @@ type GraphQLClientConnection(invoker : HttpMessageInvoker, ownsInvoker : bool) =
     /// The provided System.Net.Http.HttpMessageInvoker is not owned by the connection, so it must be disposed when it is not necessary anymore.
     new(invoker) = new GraphQLClientConnection(invoker, false)
 
+    /// Creates a new instance of GraphQLClientConnection, using a provided System.Net.Http.IHttpClientFactory to generate a System.Net.Http.HttpClient instance.
+    /// The provided System.Net.Http.HttpMessageInvoker is owned by the connection, being disposed when the connection is disposed.
+    new(factory : IHttpClientFactory) = new GraphQLClientConnection(factory.CreateClient(), true)
+
     /// Creates a new instance of GraphQLClientConnection, using a provided System.Net.Http.HttpClient as a System.Net.Http.HttpMessageInvoker to be wrapped inside.
     /// The provided System.Net.Http.HttpMessageInvoker is not owned by the connection, so it must be disposed when it is not necessary anymore.
-    new(invoker : HttpClient) = new GraphQLClientConnection(invoker, false)
+    new(client : HttpClient) = new GraphQLClientConnection(client, false)
 
     /// Creates a new instance of GraphQLClientConnection, using a fresh new instance of System.Net.Http.HttpClient as an System.Net.Http.HttpMessageInvoker.
-    /// This instance is owned by the GraphQLClientConnection, being disposed when the connection itself is disposed.
+    /// This instance is owned by the GraphQLClientConnection, being disposed when the connection is disposed.
     new() = new GraphQLClientConnection(new HttpClient(), true)
 
     /// The System.Net.Http.HttpMessageInvoker used by this connection.
