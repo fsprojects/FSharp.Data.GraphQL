b6067a2e3c1f293a1a8bbc9da848c5a89b39c739
John Berzy <john.berzy@mybazinga.com>
2021-06-28 22:20:17 -0700

    Add record serializer for pre-net5.0 compat

--- src/FSharp.Data.GraphQL.Client/Serialization.fs
+++ src/FSharp.Data.GraphQL.Client/Serialization.fs
@@ -9,6 +9,7 @@ open System.Text.Json.Serialization
 open Microsoft.FSharp.Reflection
 open System.Reflection
 open System.Collections.Generic
+open System.Collections.Concurrent
 open System.Globalization
 open FSharp.Data.GraphQL
 open FSharp.Data.GraphQL.Client.ReflectionPatterns
@@ -38,9 +39,104 @@ type OptionConverterFactory () =
             .GetConstructor([||])
             .Invoke([||]) :?> JsonConverter
 
+
+type RecordConverter<'T> () =
+    inherit JsonConverter<'T>()
+    let isNullableField (propertyInfo: PropertyInfo) =
+        let propType = propertyInfo.PropertyType
+        propType.IsGenericType &&
+            (let genericType = propType.GetGenericTypeDefinition()
+             genericType = typedefof<option<_>> || genericType = typedefof<voption<_>>)
+
+    let makeRecord = FSharpValue.PreComputeRecordConstructor(typeof<'T>, true)
+    let readRecord = FSharpValue.PreComputeRecordReader(typeof<'T>, true)
+
+    let recordFields =
+        [| for field in FSharpType.GetRecordFields(typeof<'T>) do
+           (field.Name, isNullableField field, field.PropertyType) |]
+
+    let fieldDefaults =
+        [| for (_,_,fieldType) in recordFields do
+            if fieldType.IsValueType
+            then Activator.CreateInstance(fieldType)
+            else null |]
+
+    let tryGetFieldPosition =
+        let cache = ConcurrentDictionary<bool * string, int option>()
+        fun (caseInsensitive: bool) (propertyName: string) ->
+            let key = (caseInsensitive, propertyName)
+            match cache.TryGetValue key with
+            | true, v ->
+                v
+            | false, _ ->
+                let stringCompare = if caseInsensitive then StringComparison.InvariantCultureIgnoreCase else StringComparison.InvariantCulture
+                let index = recordFields |> Array.tryFindIndex(fun (v, _, _) -> String.Equals(v, propertyName, stringCompare))
+                cache.[key] <- index
+                index
+
+    override _.Read(reader, typeToConvert, options) =
+        if reader.TokenType <> JsonTokenType.StartObject then
+            failwith "Expected Json Start Object Token"
+        let mutable cont = true
+        let values = Array.copy fieldDefaults
+        let tryGetPosition = tryGetFieldPosition options.PropertyNameCaseInsensitive
+        while cont do
+            match reader.Read(), reader.TokenType with
+            | true, JsonTokenType.EndObject ->
+                cont <- false
+            | true, JsonTokenType.PropertyName ->
+                let propertyName = reader.GetString()
+                match tryGetPosition propertyName with
+                | Some pos ->
+                    let (fieldName, isNullable, propertyType) = recordFields.[pos]
+                    let value = JsonSerializer.Deserialize(&reader, propertyType, options)
+                    if (not isNullable) && isNull value then
+                        failwithf "Field '%s' is required" fieldName
+                    values.[pos] <- value
+                | None ->
+                    reader.Skip()
+                    ()
+            | false, _ ->
+                failwith "Unexpected End of JSON Sequence"
+            | _, token ->
+                failwithf "Unexpected token '%A' in JSON Sequence" token
+        let record = makeRecord values
+        record :?> 'T
+
+
+    override _.Write(writer, value, options) =
+        writer.WriteStartObject()
+        let values = readRecord value
+        let normalizeName =
+            if isNull options.PropertyNamingPolicy
+            then id
+            else options.PropertyNamingPolicy.ConvertName
+        for i = 0 to values.Length - 1 do
+            let (name, _, propertyType) = recordFields.[i]
+            writer.WritePropertyName(normalizeName name)
+            JsonSerializer.Serialize(writer, values.[i], propertyType, options)
+        writer.WriteEndObject()
+
+type RecordConverterFactory () =
+    inherit JsonConverterFactory()
+    let recordTypeConverter = typedefof<RecordConverter<_>>
+
+    override _.CanConvert(typeToConvert) =
+        FSharpType.IsRecord typeToConvert
+
+    override _.CreateConverter(typeToConvert, options) =
+        recordTypeConverter
+            .MakeGenericType(typeToConvert)
+            .GetConstructor([||])
+            .Invoke([||]) :?> JsonConverter
+
+
+
 module Serialization =
-    let options = JsonSerializerOptions(PropertyNameCaseInsensitive=true)
+
+    let options = JsonSerializerOptions(PropertyNameCaseInsensitive=true, PropertyNamingPolicy=JsonNamingPolicy.CamelCase)
     options.Converters.Add(OptionConverterFactory())
+    options.Converters.Add(RecordConverterFactory())
     options.Converters.Add(JsonStringEnumConverter())
 
 
