ec11590c6e1bbc615664ca974b40cd5aa1b37be9
njlr <njlr@users.noreply.github.com>
2022-12-16 14:04:53 +0000

    bugfix/issue-414-enum-unpacking (#415)
    * Implemented use of enum names to pick a value
    * Modernized error raising
    * Fixed AST values error messages
    
    Co-authored-by: Andrii Chebukin <xperiandri@live.ru>

--- tests/FSharp.Data.GraphQL.Tests/ResolveTests.fs
+++ tests/FSharp.Data.GraphQL.Tests/ResolveTests.fs
@@ -11,55 +11,109 @@ open FSharp.Data.GraphQL.Parser
 open FSharp.Data.GraphQL.Execution
 
 type TestData =
-    { Test: string }
-    member x.TestMethod (a: string) (b: int) = x.Test + a + b.ToString()
-    member x.AsyncTestMethod (a: string) (b: int) = async { return x.Test + a + b.ToString() }
-let testSchema testFields : Schema<TestData> = Schema(Define.Object("Query", fields = testFields))
+    { Test : string }
+
+    member x.TestMethod (a : string) (b : int) = x.Test + a + b.ToString ()
+    member x.AsyncTestMethod (a : string) (b : int) = async { return x.Test + a + b.ToString () }
+
+let testSchema testFields : Schema<TestData> = Schema (Define.Object ("Query", fields = testFields))
 
 [<Fact>]
 let ``Execute uses default resolve to accesses properties`` () =
-    let schema = testSchema [ Define.AutoField("test", String) ]
+    let schema = testSchema [ Define.AutoField ("test", String) ]
     let expected = NameValueLookup.ofList [ "test", "testValue" :> obj ]
-    let actual = sync <| Executor(schema).AsyncExecute(parse "{ test }", { Test = "testValue" })
+
+    let actual =
+        sync
+        <| Executor(schema).AsyncExecute (parse "{ test }", { Test = "testValue" })
+
     match actual with
-    | Direct(data, errors) ->
-      empty errors
-      data.["data"] |> equals (upcast expected)
+    | Direct (data, errors) ->
+        empty errors
+        data.["data"] |> equals (upcast expected)
     | _ -> fail "Expected Direct GQResponse"
 
 [<Fact>]
 let ``Execute uses provided resolve function to accesses properties`` () =
-    let schema = testSchema [ Define.Field("test", String, "", [ Define.Input("a", String) ], resolve = fun ctx d -> d.Test + ctx.Arg("a")) ]
+    let schema =
+        testSchema [ Define.Field ("test", String, "", [ Define.Input ("a", String) ], resolve = fun ctx d -> d.Test + ctx.Arg ("a")) ]
+
     let expected = NameValueLookup.ofList [ "test", "testValueString" :> obj ]
-    let actual = sync <| Executor(schema).AsyncExecute(parse "{ test(a: \"String\") }", { Test = "testValue" })
+
+    let actual =
+        sync
+        <| Executor(schema)
+            .AsyncExecute (parse "{ test(a: \"String\") }", { Test = "testValue" })
+
     match actual with
-    | Direct(data, errors) ->
-      empty errors
-      data.["data"] |> equals (upcast expected)
+    | Direct (data, errors) ->
+        empty errors
+        data.["data"] |> equals (upcast expected)
     | _ -> fail "Expected Direct GQResponse"
 
 type private Fruit =
-  | Apple
-  | Banana
-  | Cherry
+    | Apple
+    | Banana
+    | Cherry
+    | DragonFruit
 
-let private enumType =
-  Define.Enum(
-    "Fruit",
-    [
-      Define.EnumValue("APPLE", Apple)
-      Define.EnumValue("BANANA", Banana)
-      Define.EnumValue("CHERRY", Cherry)
-    ]
-  )
+module private Fruit =
+
+    let show =
+        function
+        | Apple -> "apple"
+        | Banana -> "banana"
+        | Cherry -> "cherry"
+        | DragonFruit -> "dragon fruit"
+
+
+let private fruitType =
+    Define.Enum (
+        "Fruit",
+        [ Define.EnumValue ("APPLE", Apple)
+          Define.EnumValue ("BANANA", Banana)
+          Define.EnumValue ("CHERRY", Cherry)
+          Define.EnumValue ("DRAGON_FRUIT", DragonFruit) ]
+    )
 
 [<Fact>]
 let ``Execute resolves enums to their names`` () =
-    let schema = testSchema [ Define.Field("fruits", ListOf enumType, "", [], resolve = fun ctx d -> [ Apple; Banana; Cherry ]) ]
-    let expected = NameValueLookup.ofList [ "fruits", [ "APPLE"; "BANANA"; "CHERRY" ] :> obj ]
-    let actual = sync <| Executor(schema).AsyncExecute(parse "{ fruits() }")
+    let schema =
+        testSchema [ Define.Field ("fruits", ListOf fruitType, "", [], resolve = fun ctx d -> [ Apple; Banana; Cherry; DragonFruit ]) ]
+
+    let expected =
+        NameValueLookup.ofList [ "fruits", [ "APPLE"; "BANANA"; "CHERRY"; "DRAGON_FRUIT" ] :> obj ]
+
+    let actual = sync <| Executor(schema).AsyncExecute (parse "{ fruits() }")
+
     match actual with
-    | Direct(data, errors) ->
-      empty errors
-      data.["data"] |> equals (upcast expected)
+    | Direct (data, errors) ->
+        empty errors
+        data.["data"] |> equals (upcast expected)
+    | _ -> fail "Expected Direct GQResponse"
+
+[<Fact>]
+let ``Execute resolves enums arguments from their names`` () =
+    let schema =
+        testSchema [ Define.Field (
+                         "foo",
+                         String,
+                         "",
+                         [ Define.Input ("fruit", fruitType, defaultValue = Cherry) ],
+                         resolve =
+                             fun ctx _ ->
+                                 let fruit = ctx.Arg "fruit"
+                                 $"You asked for {Fruit.show fruit}"
+                     ) ]
+
+    let expected = NameValueLookup.ofList [ "foo", box "You asked for dragon fruit" ]
+
+    let actual =
+        sync
+        <| Executor(schema).AsyncExecute (parse "{ foo(fruit: DRAGON_FRUIT) }")
+
+    match actual with
+    | Direct (data, errors) ->
+        empty errors
+        data.["data"] |> equals (upcast expected)
     | _ -> fail "Expected Direct GQResponse"
