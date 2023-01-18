eac874d9f12a58e4020a51ba9683d970fef0bfc5
Oleg Khorsunenko <oleg.khorsunenko@lula.is>
2023-01-18 20:55:27 +0100

    LHK-590 add relay project

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+module.exports = {
+    entry: './src/app.jsx',
+    output: {
+        filename: 'bundle.js', //this is the default name, so you can skip it
+        //at this directory our bundle file will be available
+        //make sure port 8090 is used when launching webpack-dev-server
+        publicPath: 'http://localhost:8090/build'
+    },
+    module: {
+        loaders: [
+            {
+                test: /.jsx?$/,
+                loader: 'babel-loader',
+                exclude: /node_modules/
+            }
+        ]
+    },
+    externals: {
+        //don't bundle the 'react' npm package with our bundle.js
+        //but get it from a global 'React' variable
+        'react': 'React'
+    },
+    resolve: {
+        extensions: ['', '.js', '.jsx']
+    }
+}
