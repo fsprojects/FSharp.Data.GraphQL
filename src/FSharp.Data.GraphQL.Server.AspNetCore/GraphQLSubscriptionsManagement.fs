b66a0f6f12c4f2e7bb9e689ce5ab924cd589a752
valber <info@valber.net>
2023-03-19 17:41:22 +0100

    Server.AspNetCore: complement to my last commit (identation)
      Fixing identation. According to suggestion during code review.

--- src/FSharp.Data.GraphQL.Server.AspNetCore/GraphQLSubscriptionsManagement.fs
+++ src/FSharp.Data.GraphQL.Server.AspNetCore/GraphQLSubscriptionsManagement.fs
@@ -1,31 +1,31 @@
 module internal FSharp.Data.GraphQL.Server.AspNetCore.GraphQLSubscriptionsManagement
 
-  let addSubscription (id : SubscriptionId, unsubscriber : SubscriptionUnsubscriber, onUnsubscribe : OnUnsubscribeAction)
-                      (subscriptions : SubscriptionsDict) =
-    subscriptions.Add(id, (unsubscriber, onUnsubscribe))
+let addSubscription (id : SubscriptionId, unsubscriber : SubscriptionUnsubscriber, onUnsubscribe : OnUnsubscribeAction)
+                    (subscriptions : SubscriptionsDict) =
+  subscriptions.Add(id, (unsubscriber, onUnsubscribe))
 
-  let isIdTaken (id : SubscriptionId) (subscriptions : SubscriptionsDict) =
-    subscriptions.ContainsKey(id)
+let isIdTaken (id : SubscriptionId) (subscriptions : SubscriptionsDict) =
+  subscriptions.ContainsKey(id)
 
-  let executeOnUnsubscribeAndDispose (id : SubscriptionId) (subscription : SubscriptionUnsubscriber * OnUnsubscribeAction) =
-      match subscription with
-      | unsubscriber, onUnsubscribe ->
-        try
-          id |> onUnsubscribe
-        finally
-          unsubscriber.Dispose()
+let executeOnUnsubscribeAndDispose (id : SubscriptionId) (subscription : SubscriptionUnsubscriber * OnUnsubscribeAction) =
+    match subscription with
+    | unsubscriber, onUnsubscribe ->
+      try
+        id |> onUnsubscribe
+      finally
+        unsubscriber.Dispose()
 
-  let removeSubscription (id: SubscriptionId) (subscriptions : SubscriptionsDict) =
-    if subscriptions.ContainsKey(id) then
-      subscriptions.[id]
-      |> executeOnUnsubscribeAndDispose id
-      subscriptions.Remove(id) |> ignore
+let removeSubscription (id: SubscriptionId) (subscriptions : SubscriptionsDict) =
+  if subscriptions.ContainsKey(id) then
+    subscriptions.[id]
+    |> executeOnUnsubscribeAndDispose id
+    subscriptions.Remove(id) |> ignore
 
-  let removeAllSubscriptions (subscriptions : SubscriptionsDict) =
-    subscriptions
-    |> Seq.iter
-        (fun subscription ->
-            subscription.Value
-            |> executeOnUnsubscribeAndDispose subscription.Key
-        )
-    subscriptions.Clear()
+let removeAllSubscriptions (subscriptions : SubscriptionsDict) =
+  subscriptions
+  |> Seq.iter
+      (fun subscription ->
+          subscription.Value
+          |> executeOnUnsubscribeAndDispose subscription.Key
+      )
+  subscriptions.Clear()
