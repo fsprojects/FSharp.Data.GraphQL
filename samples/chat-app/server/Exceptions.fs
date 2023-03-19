a250630749855eddc40dfc6f4c6029e361762321
Valber M. Silva de Souza <valbers@gmail.com>
2023-03-19 17:44:37 +0100

    Update samples/chat-app/server/Exceptions.fs
    Summarizing module + Adjusting identation
    
    Co-authored-by: Andrii Chebukin <xperiandri@live.ru>

--- samples/chat-app/server/Exceptions.fs
+++ samples/chat-app/server/Exceptions.fs
@@ -1,26 +1,25 @@
-namespace FSharp.Data.GraphQL.Samples.ChatApp
+module FSharp.Data.GraphQL.Samples.ChatApp.Exceptions
 
-module Exceptions =
-  open FSharp.Data.GraphQL
+open FSharp.Data.GraphQL
 
-  let Member_With_This_Name_Already_Exists (theName : string) =
-    GraphQLException(sprintf "member with name \"%s\" already exists" theName)
+let Member_With_This_Name_Already_Exists (theName : string) =
+  GraphQLException(sprintf "member with name \"%s\" already exists" theName)
 
-  let Organization_Doesnt_Exist (theId : OrganizationId) =
-    match theId with
-    | OrganizationId x ->
-      GraphQLException (sprintf "organization with ID \"%s\" doesn't exist" (x.ToString()))
+let Organization_Doesnt_Exist (theId : OrganizationId) =
+  match theId with
+  | OrganizationId x ->
+    GraphQLException (sprintf "organization with ID \"%s\" doesn't exist" (x.ToString()))
 
-  let ChatRoom_Doesnt_Exist (theId : ChatRoomId) =
-    GraphQLException(sprintf "chat room with ID \"%s\" doesn't exist" (theId.ToString()))
+let ChatRoom_Doesnt_Exist (theId : ChatRoomId) =
+  GraphQLException(sprintf "chat room with ID \"%s\" doesn't exist" (theId.ToString()))
 
-  let PrivMember_Doesnt_Exist (theId : MemberPrivateId) =
-    match theId with
-    | MemberPrivateId x ->
-      GraphQLException(sprintf "member with private ID \"%s\" doesn't exist" (x.ToString()))
+let PrivMember_Doesnt_Exist (theId : MemberPrivateId) =
+  match theId with
+  | MemberPrivateId x ->
+    GraphQLException(sprintf "member with private ID \"%s\" doesn't exist" (x.ToString()))
 
-  let Member_Isnt_Part_Of_Org () =
-    GraphQLException("this member is not part of this organization")
+let Member_Isnt_Part_Of_Org () =
+  GraphQLException("this member is not part of this organization")
 
-  let ChatRoom_Isnt_Part_Of_Org () =
-    GraphQLException("this chat room is not part of this organization")
+let ChatRoom_Isnt_Part_Of_Org () =
+  GraphQLException("this chat room is not part of this organization")
