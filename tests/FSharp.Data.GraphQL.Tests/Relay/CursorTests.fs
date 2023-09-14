009acea89baa1f8faf310cf3d8218c32f53bbc93
Andrii Chebukin <andrii@lula.is>
2023-09-14 16:57:23 +0400

    Formatted Relay tests

--- tests/FSharp.Data.GraphQL.Tests/Relay/CursorTests.fs
+++ tests/FSharp.Data.GraphQL.Tests/Relay/CursorTests.fs
@@ -19,25 +19,28 @@ type User = { Id : string; Name : string; Widgets : Widget list }
 
 [<Fact>]
 let ``Relay cursor works for types with nested fileds`` () =
-    let viewer =
-        { Id = "1"
-          Name = "Anonymous"
-          Widgets =
-            [ { Id = "1"; Name = "What's it" }
-              { Id = "2"; Name = "Who's it" }
-              { Id = "3"; Name = "How's it" } ] }
+    let viewer = {
+        Id = "1"
+        Name = "Anonymous"
+        Widgets = [
+            { Id = "1"; Name = "What's it" }
+            { Id = "2"; Name = "Who's it" }
+            { Id = "3"; Name = "How's it" }
+        ]
+    }
 
-    let getUser id = if viewer.Id = id then Some viewer else None
-    let getWidget id = viewer.Widgets |> List.tryFind (fun w -> w.Id = id)
+    let tryGetUser id = if viewer.Id = id then Some viewer else None
+    let tryGetWidget id = viewer.Widgets |> List.tryFind (fun w -> w.Id = id)
 
     let rec Widget =
         Define.Object<Widget> (
             name = "Widget",
             description = "A shiny widget",
             interfaces = [ Node ],
-            fields =
-                [ Define.GlobalIdField (fun _ w -> w.Id)
-                  Define.Field ("name", StringType, (fun _ (w : Widget) -> w.Name)) ]
+            fields = [
+                Define.GlobalIdField (fun _ w -> w.Id)
+                Define.Field ("name", StringType, (fun _ (w : Widget) -> w.Name))
+            ]
         )
 
     and WidgetsField name (getUser : ResolveFieldContext -> 'a -> User) =
@@ -53,10 +56,11 @@ let ``Relay cursor works for types with nested fileds`` () =
             name = "User",
             description = "A person who uses our app",
             interfaces = [ Node ],
-            fields =
-                [ Define.GlobalIdField (fun _ w -> w.Id)
-                  Define.Field ("name", StringType, (fun _ w -> w.Name))
-                  WidgetsField "widgets" (fun _ user -> user) ]
+            fields = [
+                Define.GlobalIdField (fun _ w -> w.Id)
+                Define.Field ("name", StringType, (fun _ w -> w.Name))
+                WidgetsField "widgets" (fun _ user -> user)
+            ]
         )
 
     and Node = Define.Node<obj> (fun () -> [ User; Widget ])
@@ -64,16 +68,18 @@ let ``Relay cursor works for types with nested fileds`` () =
     let Query =
         Define.Object (
             "Query",
-            [ Define.NodeField (
-                  Node,
-                  fun _ () id ->
-                      match id with
-                      | GlobalId ("User", i) -> getUser i |> Option.map box
-                      | GlobalId ("Widget", i) -> getWidget i |> Option.map box
-                      | _ -> None
-              )
-              Define.Field ("viewer", User, (fun _ () -> viewer))
-              WidgetsField "widgets" (fun _ () -> viewer) ]
+            [
+                Define.NodeField (
+                    Node,
+                    fun _ () id ->
+                        match id with
+                        | GlobalId ("User", i) -> tryGetUser i |> Option.map box
+                        | GlobalId ("Widget", i) -> tryGetWidget i |> Option.map box
+                        | _ -> None
+                )
+                Define.Field ("viewer", User, (fun _ () -> viewer))
+                WidgetsField "widgets" (fun _ () -> viewer)
+            ]
         )
 
     let schema = Schema (query = Query, config = { SchemaConfig.Default with Types = [ User; Widget ] })
