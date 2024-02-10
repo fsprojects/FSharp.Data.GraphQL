384176f5cd57dd5bb9f38a71b6c3a4f6531ad613
Andrii Chebukin <XperiAndri@Outlook.com>
2024-02-10 20:56:30 +0400

    Added value object tests

--- tests/FSharp.Data.GraphQL.Tests/Variables and Inputs/CoercionTests.fs
+++ tests/FSharp.Data.GraphQL.Tests/Variables and Inputs/CoercionTests.fs
@@ -1,5 +1,5 @@
 // The MIT License (MIT)
-/// Copyright (c) 2015-Mar 2016 Kevin Thompson @kthompson
+// Copyright (c) 2015-Mar 2016 Kevin Thompson @kthompson
 // Copyright (c) 2016 Bazinga Technologies Inc
 
 [<UseInvariantCulture>]
