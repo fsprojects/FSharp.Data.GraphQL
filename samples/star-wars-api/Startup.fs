318901d2e6b28ab3fe92789a8706953e3818e00c
valber <info@valber.net>
2024-03-06 20:51:26 +0100

    Removed superfluous JsonOptions configuration

--- samples/star-wars-api/Startup.fs
+++ samples/star-wars-api/Startup.fs
@@ -5,20 +5,12 @@ open FSharp.Data.GraphQL.Server.AspNetCore.Giraffe
 open FSharp.Data.GraphQL.Server.AspNetCore
 open Microsoft.AspNetCore.Builder
 open Microsoft.AspNetCore.Http
-open Microsoft.AspNetCore.Http.Json
 open Microsoft.AspNetCore.Server.Kestrel.Core
 open Microsoft.Extensions.Configuration
 open Microsoft.Extensions.DependencyInjection
 open Microsoft.Extensions.Logging
 open System
-open Microsoft.AspNetCore.Server.Kestrel.Core
 open Microsoft.Extensions.Hosting
-open Microsoft.Extensions.Options
-
-// See https://learn.microsoft.com/en-us/dotnet/api/microsoft.aspnetcore.mvc.jsonoptions
-type MvcJsonOptions = Microsoft.AspNetCore.Mvc.JsonOptions
-// See https://learn.microsoft.com/en-us/dotnet/api/microsoft.aspnetcore.http.json.jsonoptions
-type HttpClientJsonOptions = Microsoft.AspNetCore.Http.Json.JsonOptions
 
 module Constants =
   let [<Literal>] Indented = "Indented"
@@ -36,25 +28,6 @@ type Startup private () =
             .AddGiraffe()
             .Configure(Action<KestrelServerOptions>(fun x -> x.AllowSynchronousIO <- true))
             .Configure(Action<IISServerOptions>(fun x -> x.AllowSynchronousIO <- true))
-            // Surprisingly minimal APIs use Microsoft.AspNetCore.Http.Json.JsonOptions
-            // Use if you want to return HTTP responses using minmal APIs IResult interface
-            .Configure<HttpClientJsonOptions>(
-                Action<HttpClientJsonOptions>(fun o ->
-                    Json.configureDefaultSerializerOptions Seq.empty o.SerializerOptions
-                )
-            )
-            // Use for pretty printing in logs
-            .Configure<HttpClientJsonOptions>(
-                Constants.Indented,
-                Action<HttpClientJsonOptions>(fun o ->
-                    Json.configureDefaultSerializerOptions Seq.empty o.SerializerOptions
-                    o.SerializerOptions.WriteIndented <- true
-                )
-            )
-            // Replace Newtonsoft.Json and use the same settings in Giraffe
-            .AddSingleton<Json.ISerializer, SystemTextJson.Serializer>(fun sp ->
-                let options = sp.GetService<IOptions<HttpClientJsonOptions>>()
-                SystemTextJson.Serializer(options.Value.SerializerOptions))
             .AddGraphQLOptions<Root>(
                 Schema.executor,
                 rootFactory,
