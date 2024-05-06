dec2fb4572aad198e19d19bd08ada717ba62d350
Andrii Chebukin <XperiAndri@Outlook.com>
2024-05-07 02:25:20 +0400

    Upgraded to the latest .NET SDK `7.0.408`

--- build/Program.fs
+++ build/Program.fs
@@ -25,7 +25,7 @@ execContext
 |> Fake.Core.Context.setExecutionContext
 
 module DotNetCli =
-    let setVersion (o : DotNet.Options) = { o with Version = Some "7.0.401" }
+    let setVersion (o : DotNet.Options) = { o with Version = Some "7.0.408" }
     let setRestoreOptions (o : DotNet.RestoreOptions) = o.WithCommon setVersion
 
 let configurationString = Environment.environVarOrDefault "CONFIGURATION" "Release"
