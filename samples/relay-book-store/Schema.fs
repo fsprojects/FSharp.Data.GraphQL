a923be5d99a744fe7717cc075b63f3a115ec3ae5
njlr <njlr@users.noreply.github.com>
2024-09-01 09:57:30 +0100

    Add comments

--- samples/relay-book-store/Schema.fs
+++ samples/relay-book-store/Schema.fs
@@ -82,12 +82,16 @@ let booksField =
                 | Some _, _, _, _ -> raise (GQLMessageException ($"Must not combine first with last or before"))
                 | _, _, Some _, _ -> raise (GQLMessageException ($"Must not combine last with first or after"))
 
+            // The total number of edges in the data-store, not the number of edges in the page!
             let totalCount = async {
                 let! count = root.FetchBooksTotalCount ()
 
                 return Some count
             }
 
+            // Workflow that fetches all of the page items plus an extra one
+            // The extra item is used to determine if there is another page
+            // This is memoized because it is used to resolve many fields, but we do not know which at this point
             let fetchItems =
                 async {
                     match sliceInfo with
@@ -96,6 +100,10 @@ let booksField =
                 }
                 |> Async.memoize
 
+            // Is there a previous page?
+            // If we are paging forward and have not specified an `after`, there can be no previous page by definition
+            // If we are paging forward with an `after`, then we must query backwards to test for a previous item
+            // If we are paging backward, we can use the extra item to determine if there is a previous page
             let hasPreviousPage = async {
                 match sliceInfo with
                 | Forward (_, None) -> return false
@@ -109,6 +117,10 @@ let booksField =
                     return List.length items > last
             }
 
+            // Is there a next page?
+            // If we are paging forward, then we can use the extra item to check
+            // If we are paging backward and have not specified an `before`, there can be no next page by definition
+            // If we are paging backward with a `before`, then we must query forwards to test for a previous item
             let hasNextPage = async {
                 match sliceInfo with
                 | Forward (first, _) ->
@@ -122,6 +134,10 @@ let booksField =
                     return List.isEmpty items |> not
             }
 
+            // The edges that are returned to the resolver
+            // According to the spec, the edge order must be the same for forward and backward pagination
+            // Our database query flips the order, so we must correct it here
+            // We also drop the extra item fetched to determine hasNextPage / hasPreviousPage
             let edges = async {
                 let! items = fetchItems
 
@@ -135,12 +151,14 @@ let booksField =
                     |> Seq.map (fun node -> { Node = node; Cursor = node |> BookCursor.ofBook |> BookCursor.encode })
             }
 
+            // The cursor of the first edge
             let startCursor = async {
                 let! edges = edges
 
                 return edges |> Seq.tryHead |> Option.map (fun x -> x.Cursor)
             }
 
+            // The cursor of the last edge
             let endCursor = async {
                 let! edges = edges
 
