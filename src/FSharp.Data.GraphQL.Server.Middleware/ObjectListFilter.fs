a38f9a6c2acfd7bcfa8a9796ef5f5f439622e8c3
Andrii Chebukin <XperiAndri@Outlook.com>
2025-03-19 22:22:33 +0400

    Fixed generation of discriminator comparing expressions to be translatable to database queries

--- src/FSharp.Data.GraphQL.Server.Middleware/ObjectListFilter.fs
+++ src/FSharp.Data.GraphQL.Server.Middleware/ObjectListFilter.fs
@@ -27,6 +27,8 @@ open System.Linq.Expressions
 open System.Runtime.InteropServices
 open System.Reflection
 
+type private CompareDiscriminatorExpression<'T, 'D> = Expression<Func<'T, 'D, bool>>
+
 /// <summary>
 /// Allows to specify discriminator comparison or discriminator getter
 /// and a function that return discriminator value depending on entity type
@@ -59,7 +61,7 @@ open System.Reflection
 /// </code></example>
 [<Struct>]
 type ObjectListFilterLinqOptions<'T, 'D>
-    ([<Optional>] compareDiscriminator : Expression<Func<'T, 'D, bool>> | null, [<Optional>] getDiscriminatorValue : (Type -> 'D) | null) =
+    ([<Optional>] compareDiscriminator : CompareDiscriminatorExpression<'T, 'D> | null, [<Optional>] getDiscriminatorValue : (Type -> 'D) | null) =
 
     member _.CompareDiscriminator = compareDiscriminator |> ValueOption.ofObj
     member _.GetDiscriminatorValue = getDiscriminatorValue |> ValueOption.ofObj
@@ -74,7 +76,7 @@ type ObjectListFilterLinqOptions<'T, 'D>
 
     new (getDiscriminator : Expression<Func<'T, 'D>>) =
         ObjectListFilterLinqOptions<'T, 'D> (ObjectListFilterLinqOptions.GetCompareDiscriminator getDiscriminator, null)
-    new (compareDiscriminator : Expression<Func<'T, 'D, bool>>) = ObjectListFilterLinqOptions<'T, 'D> (compareDiscriminator, null)
+    new (compareDiscriminator : CompareDiscriminatorExpression<'T, 'D>) = ObjectListFilterLinqOptions<'T, 'D> (compareDiscriminator, null)
     new (getDiscriminatorValue : Type -> 'D) =
         ObjectListFilterLinqOptions<'T, 'D> (compareDiscriminator = null, getDiscriminatorValue = getDiscriminatorValue)
     new (getDiscriminator : Expression<Func<'T, 'D>>, getDiscriminatorValue : Type -> 'D) =
@@ -227,6 +229,20 @@ module ObjectListFilter =
             let paramExpr = Expression.PropertyOrField (param, f.FieldName)
             buildFilterExpr (SourceExpression paramExpr) buildTypeDiscriminatorCheck f.Value
 
+    type private CompareDiscriminatorExpressionVisitor<'T, 'D> (
+        compareDiscriminator : CompareDiscriminatorExpression<'T, 'D>,
+        param : SourceExpression,
+        value : obj
+    ) =
+        inherit ExpressionVisitor ()
+        override _.VisitParameter(node) =
+            if node = compareDiscriminator.Parameters.[0] then
+                param.Value
+            elif node = compareDiscriminator.Parameters.[1] then
+                Expression.Constant(value) :> Expression
+            else
+                node :> Expression
+
     let apply (options : ObjectListFilterLinqOptions<'T, 'D>) (filter : ObjectListFilter) (query : IQueryable<'T>) =
         // Helper for discriminator comparison
         let buildTypeDiscriminatorCheck (param : SourceExpression) (t : Type) =
@@ -239,13 +255,9 @@ module ObjectListFilter =
                     Expression.Constant (t.FullName)
                 ) :> Expression
             | ValueSome discExpr, ValueNone ->
-                Expression.Invoke (
-                    // Provided discriminator comparison
-                    discExpr,
-                    param,
-                    // Default discriminator value gathered from type
-                    Expression.Constant(t.FullName)
-                ) :> Expression
+                // Replace parameters from the original expression with our new ones
+                let replacer = CompareDiscriminatorExpressionVisitor (discExpr, param, t.FullName)
+                replacer.Visit discExpr.Body
             | ValueNone, ValueSome discValueFn ->
                 let discriminatorValue = discValueFn t
                 Expression.Equal (
@@ -256,13 +268,9 @@ module ObjectListFilter =
                 ) :> Expression
             | ValueSome discExpr, ValueSome discValueFn ->
                 let discriminatorValue = discValueFn t
-                Expression.Invoke (
-                    // Provided discriminator comparison
-                    discExpr,
-                    param,
-                    // Provided discriminator value gathered from type
-                    Expression.Constant (discriminatorValue)
-                )
+                // Replace parameters from the original expression with our new ones
+                let replacer = CompareDiscriminatorExpressionVisitor (discExpr, param, discriminatorValue)
+                replacer.Visit discExpr.Body
         let queryExpr =
             let param = Expression.Parameter (typeof<'T>, "x")
             let body = buildFilterExpr (SourceExpression param) buildTypeDiscriminatorCheck filter
