709eb89aec34274c7f46fbbdc9d0b848a0d18b18
Valber M. Silva de Souza <info@valber.net>
2025-05-05 19:14:45 +0200

    fixing ws subprot. impl: sending correct payload format
      Data should be in property "data" and errors should be in property
    "errors"

--- src/FSharp.Data.GraphQL.Server.AspNetCore/GraphQLWebsocketMiddleware.fs
+++ src/FSharp.Data.GraphQL.Server.AspNetCore/GraphQLWebsocketMiddleware.fs
@@ -169,33 +169,27 @@ type GraphQLWebSocketMiddleware<'Root>
         let sendMsg = sendMessageViaSocket serializerOptions socket
         let rcv () = socket |> rcvMsgViaSocket serializerOptions
 
-        let sendOutput id (output : Output) =
-            match output.TryGetValue "errors" with
-            | true, theValue ->
-                // The specification says: "This message terminates the operation and no further messages will be sent."
-                subscriptions
-                |> GraphQLSubscriptionsManagement.removeSubscription (id)
-                sendMsg (Error (id, unbox theValue))
-            | false, _ -> sendMsg (Next (id, output))
+        let sendOutput id (output : SubscriptionExecutionResult) =
+            sendMsg (Next (id, output))
 
         let sendSubscriptionResponseOutput id subscriptionResult =
             match subscriptionResult with
-            | SubscriptionResult output -> output |> sendOutput id
+            | SubscriptionResult output -> { Data = ValueSome output; Errors = [] } |> sendOutput id
             | SubscriptionErrors (output, errors) ->
                 logger.LogWarning ("Subscription errors: {subscriptionErrors}", (String.Join ('\n', errors |> Seq.map (fun x -> $"- %s{x.Message}"))))
-                Task.FromResult ()
+                { Data = ValueNone; Errors = errors } |> sendOutput id
 
         let sendDeferredResponseOutput id deferredResult =
             match deferredResult with
             | DeferredResult (obj, path) ->
                 let output = obj :?> Dictionary<string, obj>
-                output |> sendOutput id
+                { Data = ValueSome output; Errors = [] } |> sendOutput id
             | DeferredErrors (obj, errors, _) ->
                 logger.LogWarning (
                     "Deferred response errors: {deferredErrors}",
                     (String.Join ('\n', errors |> Seq.map (fun x -> $"- %s{x.Message}")))
                 )
-                Task.FromResult ()
+                { Data = ValueNone; Errors = errors } |> sendOutput id
 
         let sendDeferredResultDelayedBy (ct : CancellationToken) (ms : int) id deferredResult : Task = task {
             do! Task.Delay (ms, ct)
@@ -208,16 +202,16 @@ type GraphQLWebSocketMiddleware<'Root>
                 (subscriptions, socket, observableOutput, serializerOptions)
                 |> addClientSubscription id sendSubscriptionResponseOutput
             | Deferred (data, errors, observableOutput) ->
-                do! data |> sendOutput id
+                do! { Data = ValueSome data; Errors = [] } |> sendOutput id
                 if errors.IsEmpty then
                     (subscriptions, socket, observableOutput, serializerOptions)
                     |> addClientSubscription id (sendDeferredResultDelayedBy cancellationToken 5000)
                 else
                     ()
-            | Direct (data, _) -> do! data |> sendOutput id
+            | Direct (data, _) -> do! { Data = ValueSome data; Errors = [] } |> sendOutput id
             | RequestError problemDetails ->
                 logger.LogWarning("Request errors:\n{errors}", problemDetails)
-
+                do! { Data = ValueNone; Errors = problemDetails } |> sendOutput id
         }
 
         let logMsgReceivedWithOptionalPayload optionalPayload (msgAsStr : string) =
