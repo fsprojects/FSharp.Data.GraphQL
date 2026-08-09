060fd26180a3048293c8f2e8245fb52260a3ca2c
Andrii Chebukin <XperiAndri@Outlook.com>
2026-08-09 22:59:02 +0200

    Updated SDK to `10.0.302`

--- build/Program.fs
+++ build/Program.fs
@@ -30,7 +30,7 @@ let ctx = Context.forceFakeContext ()
 let embedAll = ctx.Arguments |> List.exists (fun arg -> arg = BuildArguments.EmbedAll)
 
 module DotNetCli =
-    let setVersion (o : DotNet.Options) = { o with Version = Some "10.0.301" }
+    let setVersion (o : DotNet.Options) = { o with Version = Some "10.0.302" }
     let setRestoreOptions (o : DotNet.RestoreOptions) = o.WithCommon setVersion
 
 let configurationString = Environment.environVarOrDefault "CONFIGURATION" "Release"
